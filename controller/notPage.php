0d96d36641ebaff0d952be4a15462c0c1cadb64c
José León <leonrj124@gmail.com>
2022-08-09 16:39:01 -0500

    eliminar

--- controller/notPage.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class NotPage extends Controller{
-        function __construct()
-        {
-           parent::__construct();
-           $this->view->render('notPage/index');
-        }
-    }
--- core/configGeneral.php
+++
@@ -1,5 +0,0 @@
-<?php
-    const SERVERURL = "http://localhost/amosis/";
-    const COMPANY = "JHONY_CREATIVO[2020]"; 
-    const MONEDA = "S/";
-    const ZONE = "America/Guatemala";
